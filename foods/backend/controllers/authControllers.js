62e9f46fd98221533b13e76b0d034d98933020aa
shehzan001 <shehzan.abaris@gmail.com>
2026-04-09 21:57:23 +0530

    fix..

--- foods/backend/controllers/authControllers.js
+++ foods/backend/controllers/authControllers.js
@@ -29,8 +29,8 @@ export const signup = async (req, res) => {
 
         const token = await genToken(user._id)
         res.cookie("token", token, {
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7*24*60*60*1000,
             httpOnly: true
         })
@@ -57,8 +57,8 @@ export const signin = async (req, res) => {
 
         const token = await genToken(user._id)
         res.cookie("token", token, {
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7*24*60*60*1000,
             httpOnly: true
         })
@@ -150,8 +150,8 @@ export const googleAuth = async (req, res) => {
         }
         const token = await genToken(user._id)
         res.cookie("token", token, {
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7*24*60*60*1000,
             httpOnly: true
         })
